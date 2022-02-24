93dd10dc603e8b971e7283930f414a94b7fe2365
MedAmineCheikh <57319440+MedAmineCheikh@users.noreply.github.com>
2022-02-24 03:51:59 +0100

    Add files via upload

---
+++ Pidev/src/main/java/tn/esprit/spring/entities/Certificats.java
@@ -0,0 +1,39 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Certificats implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int IdCerteficat;
+	private String NomCerteficate;
+	@Temporal(TemporalType.DATE)
+	private Date Date;
+	private String Username;
+	private String Trainername;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="certificats")
+	private List<Training> trainings;
+}
